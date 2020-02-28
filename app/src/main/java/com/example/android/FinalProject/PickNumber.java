9b3fbffa4b35636b67e0caa86e8012df44170d3d
kimijiang <1017938873@qq.com>
2020-02-28 11:38:38 -0500

    implement shake shake

--- app/src/main/java/com/example/android/FinalProject/PickNumber.java
+++ app/src/main/java/com/example/android/FinalProject/PickNumber.java
@@ -10,13 +10,34 @@ import java.util.Random;
 
 public class PickNumber extends AppCompatActivity {
 
+    private ShakeHelper mShakeHelper = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pick_number);
+        mShakeHelper = new ShakeHelper(this);
+        mShakeHelper.setOnShakeListener(new ShakeHelper.OnShakeListener() {
+            @Override
+            public void onShake() {
+                shakeHandler();
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mShakeHelper.onResume();
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mShakeHelper.onPause( );
     }
 
     public void randomNumber(View view) {
+
         TextView number = findViewById(R.id.number);
         TextView from = findViewById(R.id.from);
         TextView to = findViewById(R.id.to);
@@ -26,12 +47,23 @@ public class PickNumber extends AppCompatActivity {
 
         int num = new Random().nextInt((tonum - fromnum) + 1) + fromnum;
 
-        Log.d("number", String.valueOf(num));
-
         number.setText(String.valueOf(num));
 
     }
 
-    
+    public void shakeHandler(){
+        TextView number = findViewById(R.id.number);
+        TextView from = findViewById(R.id.from);
+        TextView to = findViewById(R.id.to);
+
+        int fromnum = Integer.parseInt(from.getText().toString());
+        int tonum = Integer.parseInt(to.getText().toString());
+
+        int num = new Random().nextInt((tonum - fromnum) + 1) + fromnum;
+
+        number.setText(String.valueOf(num));
+    }
+
+
 
 }
--- app/src/main/java/com/example/android/FinalProject/YesOrNo.java
+++ app/src/main/java/com/example/android/FinalProject/YesOrNo.java
@@ -8,10 +8,41 @@ import android.widget.TextView;
 
 public class YesOrNo extends AppCompatActivity {
 
+    private ShakeHelper mShakeHelper = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_yes_or_no);
+        mShakeHelper = new ShakeHelper(this);
+        mShakeHelper.setOnShakeListener(new ShakeHelper.OnShakeListener() {
+            @Override
+            public void onShake() {
+                shakeHandler();
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mShakeHelper.onResume();
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mShakeHelper.onPause( );
+    }
+
+    private void shakeHandler() {
+        TextView display = findViewById(R.id.yon);
+
+        int num = (int) (Math.random()*10)%2;
+        if (num == 0){
+            display.setText("Yes!");
+        } else {
+            display.setText(("No!"));
+        }
     }
 
     public void startRandom(View view) {
